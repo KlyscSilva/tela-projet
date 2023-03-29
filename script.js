6225f4b8de71d54d13fb11e234815ca7a9845209
Matheus Silva <114446363+KlyscSilva@users.noreply.github.com>
2023-03-29 12:08:05 -0300

    ip

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const element = document.getElementsByClassName("button-register");
 
 function irNextPage() {
   window.location = "index2.html";
+  document.querySelector('.button-left').addEventListener('click', function() {
+   document.querySelector('.page-1').classList.add('page-1-hidden');
+   document.querySelector('.page-2').classList.add('page-2-visible');
+});
+
 }
 
 function registerPage() {
