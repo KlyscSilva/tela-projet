2ca217f1a86365ea4eb9a09a01e37c5b5a2ab0e7
KlyscSilva <smatheussilva@hotmail.com>
2023-03-29 00:57:15 -0300

    iniciando o js

---
+++ script.js
@@ -0,0 +1,9 @@
+const element = document.getElementsByClassName("button-register");
+
+function irNextPage() {
+  window.location = "index2.html";
+}
+
+function registerPage() {
+  window.location = "index.html";
+}
